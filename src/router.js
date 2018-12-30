4b43c9eb5946695f3b09dcfaeda3655804bf5084
Rebecca Peltz <rebeccapeltz@gmail.com>
2018-12-29 21:29:34 -0800

    migrate

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/views/Home'
+// TODO: Import the Survey component
+// TODO: Import the Secret component
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    }
+    // Add the Survey route definition
+    // Add the Secret route definition
+  ]
+})
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            alias: require('./aliases.config').webpack
+        }
+    },
+    runtimeCompiler: true,
+    css: {
+        sourceMap: true
+    },
+    baseUrl: '',
+    outputDir: './docs/',
+    assetsDir: 'assets'
+}
